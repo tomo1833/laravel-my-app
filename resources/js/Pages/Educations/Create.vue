b7d35d3549067a2f0a1843aa00a26e4bed8adb41
tomo1833 <m.tomo0912.tm@gmail.com>
2024-06-08 02:07:30 +0900

    quileditor full add.

--- resources/js/Pages/Educations/Create.vue
+++ resources/js/Pages/Educations/Create.vue
@@ -1,19 +1,19 @@
 <script setup lang="ts">
-import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
 import { reactive } from "vue";
 import { Head } from "@inertiajs/vue3";
 import { Inertia } from "@inertiajs/inertia";
 import { useForm } from "laravel-precognition-vue-inertia";
 import { QuillEditor } from "@vueup/vue-quill";
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+import "@vueup/vue-quill/dist/vue-quill.snow.css";
 
 const form = useForm("post", "/education", {
-    id: null,
-    title: null,
-    body: null,
-    body_html: null,
-    order: null,
+  id: null,
+  title: null,
+  body: null,
+  body_html: null,
+  order: null,
 });
-
 
 const toolbarOptions = [
   ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
@@ -35,78 +35,78 @@ const toolbarOptions = [
   ['clean']                                         // remove formatting button
 ];
 
-const storeEduations = () => {
-    console.log("----------");
-    console.log(form.body_html);
-    form.submit({
-        preserveScroll: true,
-        onSuccess: () => form.reset(),
-    });
+const storeEducations = () => {
+  console.log("----------");
+  console.log(form.body_html);
+  form.submit({
+    preserveScroll: true,
+    onSuccess: () => form.reset(),
+  });
 };
 </script>
 
+
 <template>
-    <Head title="教育ページ登録" />
+  <Head title="教育ページ登録" />
 
-    <AuthenticatedLayout>
-        <section class="text-gray-600 body-font py-24 relative">
-            <form @submit.prevent="storeEduations">
-                <div class="container px-5 py-8 mx-auto bg-white">
-                    <div class="flex flex-col text-center w-full mb-12">
-                        <h1
-                            class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
-                        >
-                            教育ページ登録
-                        </h1>
-                    </div>
+  <AuthenticatedLayout>
+    <section class="text-gray-600 body-font py-24 relative">
+      <form @submit.prevent="storeEducations">
+        <div class="container px-5 py-8 mx-auto bg-white">
+          <div class="flex flex-col text-center w-full mb-12">
+            <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">
+              教育ページ登録
+            </h1>
+          </div>
 
-                    <div class="mb-4">
-                        <label class="block mb-2">タイトル</label>
-                        <input
-                            type="text"
-                            name="title"
-                            v-model="form.title"
-                            class="p-2 border rounded w-full"
-                        />
-                        <div v-if="form.invalid('title')" class="text-red-500">
-                            {{ form.errors.title }}
-                        </div>
-                    </div>
-                    <div class="mb-4">
-                        <label class="block mb-2">本文（HTML）</label>
-                        <QuillEditor
-                            theme="snow"
-                            v-model:content="form.body_html"
-                            contentType="html"
-                            :toolbar="toolbarOptions"
-                        />
-                        <div
-                            v-if="form.invalid('body_html')"
-                            class="text-red-500"
-                        >
-                            {{ form.errors.body_html }}
-                        </div>
-                    </div>
-                    <div class="mb-4">
-                        <label class="block mb-2">表示順</label>
-                        <input
-                            type="text"
-                            name="order"
-                            v-model="form.order"
-                            class="p-2 border rounded w-full"
-                        />
-                        <div v-if="form.invalid('order')" class="text-red-500">
-                            {{ form.errors.order }}
-                        </div>
-                    </div>
-                    <button
-                        type="submit"
-                        class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-                    >
-                        登録
-                    </button>
-                </div>
-            </form>
-        </section>
-    </AuthenticatedLayout>
+          <div class="mb-4">
+            <label class="block mb-2">タイトル</label>
+            <input
+              type="text"
+              name="title"
+              v-model="form.title"
+              class="p-2 border rounded w-full"
+            />
+            <div v-if="form.invalid('title')" class="text-red-500">
+              {{ form.errors.title }}
+            </div>
+          </div>
+
+          <div class="mb-4">
+            <label class="block mb-2">本文（HTML）</label>
+            <QuillEditor
+              theme="snow"
+              v-model:content="form.body_html"
+              contentType="html"
+              :toolbar="toolbarOptions"
+            />
+            <div v-if="form.invalid('body_html')" class="text-red-500">
+              {{ form.errors.body_html }}
+            </div>
+          </div>
+
+          <div class="mb-4">
+            <label class="block mb-2">表示順</label>
+            <input
+              type="text"
+              name="order"
+              v-model="form.order"
+              class="p-2 border rounded w-full"
+            />
+            <div v-if="form.invalid('order')" class="text-red-500">
+              {{ form.errors.order }}
+            </div>
+          </div>
+
+          <button
+            type="submit"
+            class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg"
+          >
+            登録
+          </button>
+        </div>
+      </form>
+    </section>
+  </AuthenticatedLayout>
 </template>
+
