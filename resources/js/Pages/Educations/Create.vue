8b856453b5b9caad4c7b89098c4b03cf1978f8f0
tomo1833 <m.tomo0912.tm@gmail.com>
2024-06-08 02:04:30 +0900

    quileditor full add.

--- resources/js/Pages/Educations/Create.vue
+++ resources/js/Pages/Educations/Create.vue
@@ -14,6 +14,27 @@ const form = useForm("post", "/education", {
     order: null,
 });
 
+
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+  [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
+
 const storeEduations = () => {
     console.log("----------");
     console.log(form.body_html);
@@ -57,7 +78,7 @@ const storeEduations = () => {
                             theme="snow"
                             v-model:content="form.body_html"
                             contentType="html"
-                            toolbar="full"
+                            :toolbar="toolbarOptions"
                         />
                         <div
                             v-if="form.invalid('body_html')"
