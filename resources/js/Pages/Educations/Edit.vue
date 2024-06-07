e8e3bf7b7dcb39598d73f5fd94f5466532c037c1
tomo1833 <m.tomo0912.tm@gmail.com>
2024-06-08 02:10:47 +0900

    quileditor full add.

--- resources/js/Pages/Educations/Edit.vue
+++ resources/js/Pages/Educations/Edit.vue
@@ -29,6 +29,27 @@ const deleteEducation = (id) => {
         onBefore: () => confirm("削除しますか？"),
     });
 };
+
+const toolbarOptions = [
+  ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+  ['blockquote', 'code-block'],
+
+  [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+  [{ 'list': 'ordered' }, { 'list': 'bullet' }],
+  [{ 'script': 'sub' }, { 'script': 'super' }],      // superscript/subscript
+  [{ 'indent': '-1' }, { 'indent': '+1' }],          // outdent/indent
+  [{ 'direction': 'rtl' }],                         // text direction
+
+  [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+  [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+
+  [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+  [{ 'font': [] }],
+  [{ 'align': [] }],
+
+  ['clean']                                         // remove formatting button
+];
+
 </script>
 
 <template>
@@ -64,6 +85,7 @@ const deleteEducation = (id) => {
                             theme="snow"
                             v-model:content="form.body_html"
                             contentType="html"
+                            :toolbar="toolbarOptions"
                         />
                         <div
                             v-if="form.invalid('body_html')"
