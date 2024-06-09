869ae6202fbd09c74d016b1b3a4f7902bf0a7f2c
tomo1833 <m.tomo0912.tm@gmail.com>
2024-06-10 01:36:23 +0900

    メニュー修正.

--- resources/js/Components/Organisms/NaviLink.vue
+++ resources/js/Components/Organisms/NaviLink.vue
@@ -1,19 +1,142 @@
 <script setup lang="ts">
+import { ref, reactive } from "vue";
 import CommonNaviLink from "@/Components/Atoms/CommonNaviLink.vue";
+
+interface MenuItem {
+    text: string;
+    routePath?: string;
+    isLink: boolean;
+    children?: MenuItem[];
+}
+
+const menuItems = ref<MenuItem[]>([
+    {
+        text: "ダッシュボード",
+        isLink: false,
+        children: [
+            { text: "ダッシュボード", routePath: "dashboard", isLink: true },
+        ],
+    },
+    {
+        text: "勤怠管理",
+        isLink: false,
+        children: [
+            { text: "勤怠管理", routePath: "attendance.index", isLink: true },
+        ],
+    },
+    {
+        text: "教育",
+        isLink: false,
+        children: [
+            { text: "教育管理", routePath: "education.index", isLink: true },
+            {
+                text: "教育大項目",
+                routePath: "educationLarge.index",
+                isLink: true,
+            },
+        ],
+    },
+    {
+        text: "趣味",
+        isLink: false,
+        children: [
+            { text: "アニメ", routePath: "anime.index", isLink: true },
+            { text: "本", routePath: "book.index", isLink: true },
+            { text: "ブログ", routePath: "blog.index", isLink: true },
+            { text: "音楽", routePath: "music.index", isLink: true },
+            { text: "将棋", routePath: "shougi.index", isLink: true },
+            { text: "漫画", routePath: "manga.index", isLink: true },
+            { text: "映画", routePath: "movie.index", isLink: true },
+        ],
+    },
+    {
+        text: "wiki",
+        isLink: false,
+        children: [{ text: "ウィキ", routePath: "wiki.index", isLink: true }],
+    },
+    {
+        text: "その他",
+        isLink: false,
+        children: [{ text: "カメラ", routePath: "camera.index", isLink: true }],
+    },
+]);
+
+const hoverIndex = reactive({
+    index: -1,
+});
 </script>
 
 <template>
-    <CommonNaviLink routePath="dashboard" text="ダッシュボード" />
-    <CommonNaviLink routePath="attendance.index" text="勤怠管理" />
-    <CommonNaviLink routePath="education.index" text="教育管理" />
-    <CommonNaviLink routePath="anime.index" text="アニメ" />
-    <CommonNaviLink routePath="book.index" text="本" />
-    <CommonNaviLink routePath="blog.index" text="ブログ" />
-    <CommonNaviLink routePath="wiki.index" text="ウィキ" />
-    <CommonNaviLink routePath="music.index" text="音楽" />
-    <CommonNaviLink routePath="camera.index" text="カメラ" />
-    <CommonNaviLink routePath="shougi.index" text="将棋" />
-    <CommonNaviLink routePath="manga.index" text="漫画" />
-    <CommonNaviLink routePath="movie.index" text="映画" />
-    <CommonNaviLink routePath="educationLarge.index" text="教育大項目" />
+    <div class="hidden space-x-8 px-4 sm:flex">
+        <ul class="flex w-full justify-evenly p-2 bg-white text-black">
+            <li
+                v-for="(item, index) in menuItems"
+                :key="item.text"
+                class="relative w-32 text-center block box-border"
+                @mouseover="hoverIndex.index = index"
+                @mouseleave="hoverIndex.index = -1"
+            >
+                <template v-if="item.isLink">
+                    <CommonNaviLink
+                        :routePath="item.routePath"
+                        :text="item.text"
+                        class="inline-block items-center justify-center h-full px-4 py-4 hover:bg-gray-200 whitespace-nowrap w-32"
+                    />
+                </template>
+                <template v-else>
+                    <div
+                        class="inline-block items-center justify-center h-full px-6 pt-6 cursor-pointer hover:bg-gray-200 whitespace-nowrap w-32"
+                    >
+                        {{ item.text }}
+                    </div>
+                    <ul
+                        v-if="hoverIndex.index === index"
+                        class="absolute left-0 mt-1 bg-white text-black shadow-lg z-10"
+                    >
+                        <li
+                            v-for="child in item.children"
+                            :key="child.text"
+                            class="whitespace-nowrap w-32 text-center block h-12"
+                        >
+                            <template v-if="child.isLink">
+                                <CommonNaviLink
+                                    :routePath="child.routePath"
+                                    :text="child.text"
+                                    class="block px-4 pt-2 hover:bg-gray-200 whitespace-nowrap w-32 h-12"
+                                />
+                            </template>
+                            <template v-else>
+                                <span
+                                    class="block px-4 hover:bg-gray-200 whitespace-nowrap"
+                                >
+                                    {{ child.text }}
+                                </span>
+                            </template>
+                        </li>
+                    </ul>
+                </template>
+            </li>
+        </ul>
+    </div>
 </template>
+
+<style scoped>
+ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+li {
+    position: relative;
+}
+
+.common-navi-link {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100%;
+    padding: 0 16px;
+    white-space: nowrap;
+}
+</style>
--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -26,9 +26,8 @@ defineProps({
                 <div
                     class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg"
                 >
-
                     <section class="text-gray-600 body-font">
-                        <div class="container px-5 py-24 mx-auto ">
+                        <div class="container px-5 py-24 mx-auto">
                             <div class="flex flex-col text-center w-full mb-20">
                                 <h1
                                     class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900"
@@ -39,7 +38,9 @@ defineProps({
                                     class="lg:w-2/3 mx-auto leading-relaxed text-base"
                                 ></p>
 
-                                <div class="flex flex-wrap m-4 text-center justify-between">
+                                <div
+                                    class="flex flex-wrap m-4 text-center justify-between"
+                                >
                                     <div class="p-4 md:w-1/4 sm:w-1/2 w-full">
                                         <div
                                             class="border-2 border-gray-200 px-4 py-6 rounded-lg"
